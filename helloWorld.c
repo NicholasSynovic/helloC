a04ed0ba236a8eb1e17d921325008e7e0f18fa0c
Nicholas M. Synovic <nicholas.synovic@gmail.com>
2022-06-28 20:28:34 -0500

    Add other ways of spitting out hello world

--- helloWorld.c
+++ helloWorld.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(void)  {
-    printf("Hello World\n");
-    return 0;
+    printf("Hello, World! (Using printf)\n");
+    puts("Hello, World! (Using puts)");
+    return EXIT_SUCCESS;    // defined in <stdlib.h>
+    // return 0;
 }
